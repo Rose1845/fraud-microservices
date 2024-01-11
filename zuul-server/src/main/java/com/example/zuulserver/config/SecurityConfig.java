a9ade670e7a21ee25657880fdb1719e9e2d8a648
Rose1845 <odhiamborose466@gmail.com>
2024-01-11 09:22:52 +0300

    secure microservice with keycloak

--- zuul-server/src/main/java/com/example/zuulserver/config/SecurityConfig.java
+++ zuul-server/src/main/java/com/example/zuulserver/config/SecurityConfig.java
@@ -1,24 +1,24 @@
-//package com.example.zuulserver.config;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.apache.hc.core5.http.impl.nio.ServerHttp1IOEventHandlerFactory;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.authentication.AbstractAuthenticationToken;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.web.server.ServerHttpSecurity;
-//import org.springframework.security.web.server.SecurityWebFilterChain;
-//import reactor.core.publisher.Mono;
-//@EnableWebSecurity
-//@Configuration
-//@RequiredArgsConstructor
-//public class SecurityConfig {
-//    @Bean
-//    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity security){
-//        security.csrf(ServerHttpSecurity.CsrfSpec::disable)
-//                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.pathMatchers("/eureka/**").permitAll().anyExchange().authenticated()).oauth2ResourceServer((Customizer<ServerHttpSecurity.OAuth2ResourceServerSpec>) jwtAuthConverter);
-//        return   security.build();
-//
-//    }
-//}
+package com.example.zuulserver.config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.hc.core5.http.impl.nio.ServerHttp1IOEventHandlerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity security){
+        security.csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.pathMatchers("/eureka/**").permitAll().anyExchange().authenticated()).oauth2ResourceServer((oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults())));
+        return   security.build();
+
+    }
+}
